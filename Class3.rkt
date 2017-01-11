9b25b81ca9cdcff4ac2f228af0b670cdfc87b33c
Kendall Weistroffer <kendall.fonebone@gmail.com>
2017-01-11 15:10:10 -0700

    fixing loop and adding whitespace handling

--- Class3.rkt
+++ Class3.rkt
@@ -10,6 +10,7 @@
 (define-tokens value-tokens (NUMBER ID))
 (define-empty-tokens operators (ADD MULT LPAREN RPAREN))
 (define-empty-tokens keywords (NI NEEWOM))
+(define-empty-tokens end (EOF))
 
 
 (define calclexer
@@ -17,14 +18,21 @@
    ["ni" (token-NI)]
    ["neewom" (token-NEEWOM)]
    [(:+ numeric) (token-NUMBER lexeme)]; ":+" means 1 or more
-   [(:: alphabetic (:*(:or alphabetic numeric))) (token-ID lexeme)] ; "::" means concatenate 
+   [(:: alphabetic (:*(:or alphabetic numeric))) (token-ID lexeme)]; "::" means concatenate
+   [whitespace (calclexer input-port)]
+   [(eof) (token-EOF)]
+   
    ))
 
 (define (lexstr str)
-  (let ([in (open-input-string str)])
-    (calclexer in)))
+  (letrec ([in (open-input-string str)]
+        [lexfun (λ (i) (let ([tok (calclexer i)])
+                         (cond [(eq? tok (token-EOF)) '()]
+                               [else (cons tok (lexfun i))])))])
+    (lexfun in)))
 
-(check-expect (lexstr "ni") (token-NI))
-(check-expect (lexstr "5") (token-NUMBER "5"))
+;(check-expect (lexstr "ni") (token-NI))
+;(check-expect (lexstr "5") (token-NUMBER "5"))
+;(check-expect (lexstr "5") (token-NUMBER "56"))
 
 (test)
