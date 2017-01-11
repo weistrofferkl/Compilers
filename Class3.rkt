17720f41907968746b79873dfc4cc2f9b5eece40
Kendall Weistroffer <kendall.fonebone@gmail.com>
2017-01-11 14:49:54 -0700

    Adding Neewom, numbers, and IDs

--- Class3.rkt
+++ Class3.rkt
@@ -1,15 +1,30 @@
 #lang racket
 ; Goal of Lexer is to generate tokens
+;lexeme - grabs whatever was matched and returns it
 
 (require parser-tools/lex
          parser-tools/yacc
          (prefix-in : parser-tools/lex-sre))
+(require test-engine/racket-tests)
 
 (define-tokens value-tokens (NUMBER ID))
 (define-empty-tokens operators (ADD MULT LPAREN RPAREN))
-define-empty-tokens keywords (NI NEEWOM))
+(define-empty-tokens keywords (NI NEEWOM))
 
 
 (define calclexer
   (lexer
-   ["ni" (token-NI)]))
+   ["ni" (token-NI)]
+   ["neewom" (token-NEEWOM)]
+   [(:+ numeric) (token-NUMBER lexeme)]; ":+" means 1 or more
+   [(:: alphabetic (:*(:or alphabetic numeric))) (token-ID lexeme)] ; "::" means concatenate 
+   ))
+
+(define (lexstr str)
+  (let ([in (open-input-string str)])
+    (calclexer in)))
+
+(check-expect (lexstr "ni") (token-NI))
+(check-expect (lexstr "5") (token-NUMBER "5"))
+
+(test)
