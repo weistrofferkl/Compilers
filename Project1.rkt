13b225f77e7b3fb67d61ba0b71e825e4ca9e3384
Kendall Weistroffer <kendall.fonebone@gmail.com>
2017-01-16 13:56:10 -0700

    Defining Tokens

--- Project1.rkt
+++ Project1.rkt
@@ -1,1 +1,18 @@
 #lang racket
+(require parser-tools/lex
+         parser-tools/yacc
+         (prefix-in : parser-tools/lex-sre))
+
+(define-tokens value-tokens (NUM ID STRING))
+(define-empty-tokens paren-types (LPAREN RPAREN LBRACKET RBRACKET LBRACE RBRACE))
+(define-empty-tokens operators (ADD MULT DIV SUB DOT))
+(define-empty-tokens punctuation (COMMA COLON SEMI))
+(define-empty-tokens comparators (EQ NE LT GT LE GE))
+(define-empty-tokens boolops (BOOLOR BOOLAND))
+
+(define-empty-tokens keywords (AND ARRAY AS BREAK DO ELSE END IF IN IS
+ JUNIPER KIND LET NEEWOM NI NOW OF PENG THEN
+ TO WHILE WITH))
+
+(define-empty-tokens endoffile (EOF))
+
