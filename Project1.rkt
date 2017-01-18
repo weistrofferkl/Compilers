7289a731593129d58c662d2ac965aada9f795ca4
Kendall Weistroffer <kendall.fonebone@gmail.com>
2017-01-18 14:12:35 -0700

    Adding ID and STRING

--- Project1.rkt
+++ Project1.rkt
@@ -2,8 +2,9 @@
 (require parser-tools/lex
          parser-tools/yacc
          (prefix-in : parser-tools/lex-sre))
+(require racket/cmdline)
 
-(define-tokens value-tokens (NUM ID STRING)) ;NEED ID, STRING
+(define-tokens value-tokens (NUM ID STRING)) ;NEED STRING
 (define-empty-tokens paren-types (LPAREN RPAREN LBRACKET RBRACKET LBRACE RBRACE)) ;done
 (define-empty-tokens operators (ADD MULT DIV SUB DOT)) ;done
 (define-empty-tokens punct (COMMA COLON SEMI)) ; done
@@ -12,13 +13,14 @@
 
 (define-empty-tokens keywords (AND ARRAY AS BREAK DO ELSE END IF IN IS ;done
  JUNIPER KIND LET NEEWOM NI NOW OF PENG THEN
- TO WHILE WITH))
+ TO WHILE WITH DEFINE))
 
 (define-empty-tokens endoffile (EOF)) ;done
 
 (define nilexer
   (lexer
    ;Keywords
+   ["define" (token-DEFINE)]
    ["neewom" (token-NEEWOM)]
    ["ni" (token-NI)]
    ["and" (token-AND)]
@@ -42,18 +44,18 @@
    ["while" (token-WHILE)]
    ["with" (token-WITH)]
    ;Paren Types
-   ["#/(" (token-LPAREN)]
-   ["#/)" (token-RPAREN)]
-   ["#/[" (token-LBRACKET)]
-   ["#/]" (token-RBRACKET)]
-   ["#/{" (token-LBRACE)]
-   ["#/}" (token-RBRACE)]
+   [#\( (token-LPAREN)]
+   [#\) (token-RPAREN)]
+   [#\[ (token-LBRACKET)]
+   [#\] (token-RBRACKET)]
+   [#\{ (token-LBRACE)]
+   [#\} (token-RBRACE)]
    ;Operators ADD MULT DIV SUB DOT
-   ["#/+" (token-ADD)]
-   ["#/*" (token-MULT)]
-   ["#//" (token-DIV)]
-   ["#/-" (token-SUB)]
-   ["#/." (token-DOT)]
+   [#\+ (token-ADD)]
+   [#\* (token-MULT)]
+   [#\/ (token-DIV)]
+   [#\-  (token-SUB)]
+   [#\. (token-DOT)]
    ;Comparators EQ NE LT GT LE GE
    ["#/=" (token-EQ)]
    ["#/<>" (token-NE)]
@@ -62,18 +64,36 @@
    ["#/<=" (token-LE)]
    ["#/>=" (token-GE)]
    ;boolops:BOOLOR BOOLAND
-   ["#/|" (token-BOOLOR)]
-   ["#/&" (token-BOOLAND)]
-   
-   
-   
+   [#\| (token-BOOLOR)]
+   [#\& (token-BOOLAND)]
    ;Punctuation COMMA COLON SEMI
-   ["#/," (token-COMMA)]
-   ["#/:" (token-COLON)]
-   ["#/;" (token-SEMI)]
-
+   [#\, (token-COMMA)]
+   [#\: (token-COLON)]
+   [#\; (token-SEMI)]
+   ;Number
    [(:+ numeric) (token-NUM lexeme)]; ":+" means 1 or more
+   ;ID
+   [(:: alphabetic (:seq (:* (:or alphabetic numeric #\_ #\-)) (:* #\'))) (token-ID lexeme)];
+   ;String - Match first quote, everything that's not a \" and then the final end quote
+   [(:: #\" (complement (:: #\/ #\")) #\")(token-STRING lexeme)]
+   ;Whitespace
    [whitespace (nilexer input-port)]
+   ;EOF
    [(eof) (token-EOF)]
+  
    ))
+
+(define (lexstr str)
+  (letrec ([in (open-input-string str)]
+        [lexfun (λ (i) (let ([tok (nilexer i)])
+                         (cond [(eq? tok (token-EOF)) '()]
+                               [else (cons tok (lexfun i))])))])
+    (lexfun in)))
+
+(define (lexfile filename)
+    (letrec ([in (open-input-file filename)]
+        [lexfun (λ (i) (let ([tok (nilexer i)])
+                         (cond [(eq? tok (token-EOF)) '()]
+                               [else (cons tok (lexfun i))])))])
+    (lexfun in)))
    
